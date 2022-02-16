aaa52b55159f13297a4130ab74e837b56d8fe6f0
dan <daniel.gierse@gmail.com>
2022-02-16 23:23:54 +0100

    folder restructure, pipeline3 added

---
+++ code/pipeline4/process_video.sh
@@ -0,0 +1,11 @@
+INPUT_FOLDER="../../testvideos/"
+OUTPUT_FOLDER="./output/"
+MODELS_FOLDER="../../models/"
+
+python process_video.py \
+	--input ${INPUT_FOLDER} \
+	--output ${OUTPUT_FOLDER} \
+	--models ${MODELS_FOLDER}
+
+	
+
